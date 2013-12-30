c867c0c5f36ed003880d99b3ec85337c6f1feb2d
TheMiles <miles@coders-of-the-caribbean.org>
2013-12-30 02:34:23 +0100

    added SERIAL_DEVICE to test script

--- test_serial.sh
+++ test_serial.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 SLEEP=0.02
+SERIAL_DEVICE=/dev/ttyUSB0
 
 
-#echo -en "\x42\x05" > /dev/ttyUSB0
+#echo -en "\x42\x05" > $SERIAL_DEVICE
 while true; do
-    #echo -en "\x42\x02\x5F\x00\x00" > /dev/ttyUSB0
-    echo -en "\x42\x02\xFF\xFF\xFF" > /dev/ttyUSB0
+    #echo -en "\x42\x02\x5F\x00\x00" > $SERIAL_DEVICE
+    echo -en "\x42\x02\xFF\xFF\xFF" > $SERIAL_DEVICE
     sleep $SLEEP
-    echo -en "\x42\x02\x00\x00\x00" > /dev/ttyUSB0
+    echo -en "\x42\x02\x00\x00\x00" > $SERIAL_DEVICE
     sleep $SLEEP
 done
