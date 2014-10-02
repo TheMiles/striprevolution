3342950bffe5a86883767b27cb8846a69b97a5bc
jmechnich <joerg.mechnich@gmail.com>
2014-10-02 12:36:28 +0200

    OSX fixes

--- sinus.py
+++ sinus.py
@@ -1,12 +1,16 @@
+#!/usr/bin/env python
+
 import colorsys, math, serial, signal, sys, time, os
-# import alsaaudio, time, audioop
+
+# does not work on OSX
+try:
+    import alsaaudio, audioop
+except:
+    pass
 
 speed = 9600
 
 ports = ["/dev/ttyUSB0","/dev/tty.usbserial-A4006Fho"]
-
-# port = "/dev/ttyUSB0"
-conn = None
 
 num_leds=5
 max_intensity=0xf
@@ -51,42 +55,42 @@ class Rainbow:
                      for j in colorsys.hsv_to_rgb(i, *(self.hsv[1:3]))]
         return bytearray(msg)
 
-# class AudioTest:
-#     def __init__(self,max,nleds):
-#         self.max   = max
-#         self.nleds = nleds
-#         self.inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE,alsaaudio.PCM_NONBLOCK)
-#         self.inp.setchannels(1)
-#         self.inp.setrate(8000)
-#         self.inp.setformat(alsaaudio.PCM_FORMAT_S16_LE)   
-#         self.inp.setperiodsize(160)
-#         self.decay = 0.05
-#         self.val = 0
-#         self.audioMin = 1
-#         self.audioMax = 0
+class AudioTest:
+    def __init__(self,max,nleds):
+        self.max   = max
+        self.nleds = nleds
+        self.inp = alsaaudio.PCM(alsaaudio.PCM_CAPTURE,alsaaudio.PCM_NONBLOCK)
+        self.inp.setchannels(1)
+        self.inp.setrate(8000)
+        self.inp.setformat(alsaaudio.PCM_FORMAT_S16_LE)   
+        self.inp.setperiodsize(160)
+        self.decay = 0.05
+        self.val = 0
+        self.audioMin = 1
+        self.audioMax = 0
 
 
-#     def iterate(self):
-#         # Read data from device
-#         l,data = self.inp.read()
-#         while not l:
-#             time.sleep(.001)
-#             l,data = self.inp.read()
-#             print type(data)
-#         val = float(audioop.max(data, 2))
-#         if val < self.audioMin: self.audioMin = val
-#         if val > self.audioMax: self.audioMax = val
-#         mid = (self.audioMin+self.audioMax)/2
-#         wid = self.audioMax-self.audioMin
-#         if wid < 1e-6:
-#             wid = 1
-#         val = (val-mid)/wid+0.5
-#         if val > self.val: self.val = val
-#         msg = [0x42, 0x02]
-#         for i in xrange(3):
-#             msg += [ int(self.val*self.max) ]
-#         self.val -= self.decay
-#         return bytearray(msg)
+    def iterate(self):
+        # Read data from device
+        l,data = self.inp.read()
+        while not l:
+            time.sleep(.001)
+            l,data = self.inp.read()
+            print type(data)
+        val = float(audioop.max(data, 2))
+        if val < self.audioMin: self.audioMin = val
+        if val > self.audioMax: self.audioMax = val
+        mid = (self.audioMin+self.audioMax)/2
+        wid = self.audioMax-self.audioMin
+        if wid < 1e-6:
+            wid = 1
+        val = (val-mid)/wid+0.5
+        if val > self.val: self.val = val
+        msg = [0x42, 0x02]
+        for i in xrange(3):
+            msg += [ int(self.val*self.max) ]
+        self.val -= self.decay
+        return bytearray(msg)
 
 
 doIterate = True
