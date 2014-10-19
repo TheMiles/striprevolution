6b3fdc2633d58340f56a6bd625bdf5f4df442f9f
jmechnich <joerg.mechnich@gmail.com>
2014-10-19 14:17:57 +0200

    put commands in separate header which is easy to parse from python

---
+++ Commands.h
@@ -0,0 +1,20 @@
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+#define COMMAND_NOP          0x00
+#define COMMAND_COLOR        0x01
+#define COMMAND_UNICOLOR     0x02
+#define COMMAND_SINGLE_COLOR 0x03
+#define COMMAND_BRIGHT       0x04
+#define COMMAND_RAINBOW      0x05
+#define COMMAND_STATE        0x06
+#define COMMAND_TEST         0x61
+#define COMMAND_TESTRAW      0x62
+#define COMMAND_CONF         0x67
+#define COMMAND_DEBUG        0x68
+#define COMMAND_RESET        0x69
+#define COMMAND_SETSIZE      0x70
+#define COMMAND_PING         0x71
+#define COMMAND_MEMFREE      0x72
+
+#endif
